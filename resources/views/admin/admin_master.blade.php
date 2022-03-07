9db9b2746b599fe00c87ae1ade21284f1dcb083f
FreshDev <billyhans90@gmail.com>
2022-03-07 16:25:38 +0100

    Admin master view complete

--- resources/views/admin/admin_master.blade.php
+++ resources/views/admin/admin_master.blade.php
@@ -6,16 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../images/favicon.ico">
+    <link rel="icon" href="{{ asset('backend/images/favicon.ico') }}">
 
-    <title>Sunny Admin - Dashboard</title>
+    <title>FreshDev School Management System</title>
     
 	<!-- Vendors Style-->
-	<link rel="stylesheet" href="css/vendors_css.css">
+	<link rel="stylesheet" href="{{ asset('backend/css/vendors_css.css') }}">
 	  
 	<!-- Style-->  
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/skin_color.css">
+	<link rel="stylesheet" href="{{ asset('backend/css/style.css') }}">
+	<link rel="stylesheet" href="{{ asset('backend/css/skin_color.css') }}">
      
   </head>
 
@@ -1241,15 +1241,15 @@
   	
 	 
 	<!-- Vendor JS -->
-	<script src="js/vendors.min.js"></script>
-    <script src="../assets/icons/feather-icons/feather.min.js"></script>	
-	<script src="../assets/vendor_components/easypiechart/dist/jquery.easypiechart.js"></script>
-	<script src="../assets/vendor_components/apexcharts-bundle/irregular-data-series.js"></script>
-	<script src="../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js"></script>
+	<script src="{{ asset('backend/js/vendors.min.js') }}"></script>
+    <script src="{{ asset('../assets/icons/feather-icons/feather.min.js') }}"></script>	
+	<script src="{{ asset('../assets/vendor_components/easypiechart/dist/jquery.easypiechart.js') }}"></script>
+	<script src="{{ asset('../assets/vendor_components/apexcharts-bundle/irregular-data-series.js') }}"></script>
+	<script src="{{ asset('../assets/vendor_components/apexcharts-bundle/dist/apexcharts.js') }}"></script>
 	
 	<!-- Sunny Admin App -->
-	<script src="js/template.js"></script>
-	<script src="js/pages/dashboard.js"></script>
+	<script src="{{ asset('backend/js/template.js') }}"></script>
+	<script src="{{ asset('backend/js/pages/dashboard.js') }}"></script>
 	
 	
 </body>
