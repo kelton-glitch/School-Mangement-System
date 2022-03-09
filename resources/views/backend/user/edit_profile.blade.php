803ce2bd06b474353bf4fde1d140c26e75e4c62a
FreshDev <billyhans90@gmail.com>
2022-03-09 17:00:35 +0100

    User profile image upload,edit,update db part 3

--- resources/views/backend/user/edit_profile.blade.php
+++ resources/views/backend/user/edit_profile.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
 <div class="content-wrapper">
@@ -87,7 +88,7 @@
                                                     <h5>Gender <span class="text-danger">*</span></h5>
                                                     <div class="controls">
                                                         <select name="gender" id="gender" required="" class="form-control">
-                                                            <option value="" selected="" disabled="">Select Role</option>
+                                                            <option value="" selected="" disabled="">Select Gender</option>
                                                             <option value="Male" {{ ($editData->gender == "Male" ? "selected": "") }}>Male</option>
                                                             <option value="Female" {{ ($editData->gender == "Female" ? "selected": "") }}>Female</option>
 
@@ -98,13 +99,24 @@
                                             </div> <!-- End col 6 -->
                                             <div class="col-md-6">
                                                 <div class="form-group">
-                                                    <h5>Profile Image <span class="text-danger">*</span></h5>
+                                                    <h5>Profile Image </h5>
                                                     <div class="controls">
-                                                        <input type="file" name="image" class="form-control" required="">
+                                                        <input type="file" name="image" class="form-control" id="image">
 
                                                     </div>
 
                                                 </div>
+
+                                                <div class="form-group">
+
+                                                    <div class="controls">
+                                                        <img id="showImage" src="{{ (!empty($user->image))? url('upload/user_images/'.$user->image):url('upload/no_image.jpg') }}" style="width:100px; width:100px; border:1px solid #000" alt="">
+
+                                                    </div>
+
+                                                </div>
+
+
                                             </div> <!-- End col 6 -->
                                         </div> <!-- End row -->
 
@@ -127,6 +139,18 @@
 
     </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready(function(){
+        $('#image').change(function(e){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                $('#showImage').attr('src',e.target.result);
+            }
+            reader.readAsDataURL(e.target.files['0']);
+        });
+    });
+</script>
 
 
 @endsection
