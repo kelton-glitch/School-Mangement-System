46235b03534199fe9eab9cd1b0838e80d23adcca
FreshDev <billyhans90@gmail.com>
2022-03-14 06:50:54 +0100

    Student registration part 5

--- app/Http/Controllers/Backend/Student/StudentRegController.php
+++ app/Http/Controllers/Backend/Student/StudentRegController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Backend\Student;
 
 use App\Http\Controllers\Controller;
 use App\Models\AssignStudent;
+use App\Models\StudentClass;
+use App\Models\StudentYear;
 use Illuminate\Http\Request;
 
 class StudentRegController extends Controller
@@ -16,6 +18,9 @@ class StudentRegController extends Controller
 
     public function StudentRegAdd()
     {
-        return view('backend.student.student_reg.student_add');
+        $data['years'] = StudentYear::all();
+        $data['classes'] = StudentClass::all();
+        $data['years'] = StudentYear::all();
+        return view('backend.student.student_reg.student_add', $data);
     }
 }
--- resources/views/backend/student/student_reg/student_add.blade.php
+++ resources/views/backend/student/student_reg/student_add.blade.php
@@ -24,19 +24,158 @@
                                 <div class="row">
                                     <div class="col-12">
 
-                                        
+                                        <div class="row">
+                                            <!-- 1st Row -->
+                                            <div class="col-md-4">
+
                                                 <div class="form-group">
-                                                    <h5>Student Year Name <span class="text-danger">*</span></h5>
+                                                    <h5>Student Name <span class="text-danger">*</span></h5>
                                                     <div class="controls">
-                                                        <input type="text" name="name" class="form-control">
-                                                        @error('name')
-                                                            <span class="text-danger">{{ $message }}</span>
-                                                        @enderror
-
+                                                        <input type="text" name="name" class="form-control" required="">
                                                     </div>
-
                                                 </div>
-                                            
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Father's Name <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="fname" class="form-control" required="">
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Mother's Name <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="mname" class="form-control" required="">
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                        </div> <!-- End 1st Row -->
+
+
+                                        <div class="row">
+                                            <!-- 2nd Row -->
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Mobile Number <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="mobile" class="form-control" required="">
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Address <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="address" class="form-control" required="">
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Gender <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <select name="gender" id="gender" required="" class="form-control">
+                                                            <option value="" selected="" disabled="">Select Gender</option>
+                                                            <option value="Male">Male</option>
+                                                            <option value="Female">Female</option>
+
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                        </div> <!-- End 2nd Row -->
+
+                                        <div class="row">
+                                            <!-- 3rd Row -->
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Date of Birth <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="date" name="dob" class="form-control" required="">
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Discount <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <input type="text" name="discount" class="form-control" required="">
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Year <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <select name="gender" id="gender" required="" class="form-control">
+                                                            <option value="" selected="" disabled="">Select Year</option>
+                                                            @foreach ($years as $year)
+                                                            <option value="{{ $year->id }}">{{ $year->name }}</option>
+
+                                                            @endforeach
+                                                            
+
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+                                        </div> <!-- End 3rd Row -->
+
+                                        <div class="row">
+                                            <!-- 4th Row -->
+                                            <div class="col-md-4">
+
+                                                <div class="form-group">
+                                                    <h5>Class <span class="text-danger">*</span></h5>
+                                                    <div class="controls">
+                                                        <select name="gender" id="gender" required="" class="form-control">
+                                                            <option value="" selected="" disabled="">Select Class</option>
+                                                            @foreach ($classes as $class)
+                                                            <option value="{{ $class->id }}">{{ $class->name }}</option>
+
+                                                            @endforeach
+                                                            
+
+                                                        </select>
+                                                    </div>
+                                                </div>
+
+                                            </div> <!-- End Col-md-4 -->
+
+
+
+                                        </div> <!-- End 4th Row -->
+
+
+
 
                                         <div class="text-xs-right">
                                             <input type="submit" class="btn btn-rounded btn-info mb-5" value="Submit">
