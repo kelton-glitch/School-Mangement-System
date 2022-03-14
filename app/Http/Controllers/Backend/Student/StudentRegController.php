77d99a6cb5f60464a9ed23f9682b244990ebe30f
FreshDev <billyhans90@gmail.com>
2022-03-14 11:24:51 +0100

    Student registration part 8

--- app/Http/Controllers/Backend/Student/StudentRegController.php
+++ app/Http/Controllers/Backend/Student/StudentRegController.php
@@ -30,7 +30,7 @@ class StudentRegController extends Controller
     {
         DB::transaction(function() use($request) {
             $checkYear = StudentYear::find($request->year_id)->name;
-            $student = User::where('usertype','student')->orderBy('id','DESC')->first();
+            $student = User::where('usertype','Student')->orderBy('id','DESC')->first();
 
             if($student == null) {
                 $firstReg = 0;
@@ -45,7 +45,7 @@ class StudentRegController extends Controller
                     $id_no = '0'.$studentId;
                 } //End 2nd If
                 else{
-                    $student = User::where('usertype','student')->orderBy('id','DESC')->first()->id;
+                    $student = User::where('usertype','Student')->orderBy('id','DESC')->first()->id;
                     $studentId = $student+1;
 
                     if($studentId < 10) {
@@ -59,6 +59,29 @@ class StudentRegController extends Controller
                     }
 
                 } //End Else
+
+                $final_id_no = $checkYear.$id_no;
+                $user = new User();
+                $code = rand(00000000,99999999);
+                $user->id_no = $final_id_no;
+                $user->password = bcrypt($code);
+                $user->usertype = 'Student';
+                $user->code = $code;
+                $user->name = $request->name;
+                $user->fname = $request->fname;
+                $user->mname = $request->mname;
+                $user->mobile = $request->mobile;
+                $user->address = $request->address;
+                $user->gender = $request->gender;
+                $user->dob = date('Y-m-d',strtotime($request->dob));
+
+                if($request->file('image')) {
+                    $file = $request->file('image');
+                        $filename = date('YmdHi').$file->getClientOriginalName();
+                    $file->move(public_path('upload/student_images'),$filename);
+                    $user['image'] = $filename;
+                }
+                $user->save();
                 
             } //End 1st If
         });
