92cc1a8be5bd823b5644e0633e74f7816eeaefb9
FreshDev <billyhans90@gmail.com>
2022-03-14 11:04:03 +0100

    Student registration part 7

--- app/Http/Controllers/Backend/Student/StudentRegController.php
+++ app/Http/Controllers/Backend/Student/StudentRegController.php
@@ -7,6 +7,8 @@ use App\Models\AssignStudent;
 use App\Models\StudentClass;
 use App\Models\StudentYear;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 
 class StudentRegController extends Controller
 {
@@ -26,6 +28,39 @@ class StudentRegController extends Controller
 
     public function StudentRegStore(Request $request)
     {
-        # code...
-    }
+        DB::transaction(function() use($request) {
+            $checkYear = StudentYear::find($request->year_id)->name;
+            $student = User::where('usertype','student')->orderBy('id','DESC')->first();
+
+            if($student == null) {
+                $firstReg = 0;
+                $studentId = $firstReg+1;
+                if($studentId < 10) {
+                    $id_no = '0000'.$studentId;
+                } elseif($studentId<100){
+                    $id_no = '000'.$studentId;
+                } elseif($studentId<1000) {
+                    $id_no = '00'.$studentId;
+                } elseif($studentId<10000) {
+                    $id_no = '0'.$studentId;
+                } //End 2nd If
+                else{
+                    $student = User::where('usertype','student')->orderBy('id','DESC')->first()->id;
+                    $studentId = $student+1;
+
+                    if($studentId < 10) {
+                        $id_no = '0000'.$studentId;
+                    } elseif($studentId<100){
+                        $id_no = '000'.$studentId;
+                    } elseif($studentId<1000) {
+                        $id_no = '00'.$studentId;
+                    } elseif($studentId<10000) {
+                        $id_no = '0'.$studentId;
+                    }
+
+                } //End Else
+                
+            } //End 1st If
+        });
+    } // End Method
 }
