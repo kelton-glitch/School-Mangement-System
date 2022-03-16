5a70e7375b4b116bc2b7c40c1edd9ac698eaa96a
FreshDev <billyhans90@gmail.com>
2022-03-16 23:20:19 +0100

    finally downloaded the pdf package

--- config/app.php
+++ config/app.php
@@ -163,7 +163,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        niklasravnsborg\LaravelPdf\PdfServiceProvider::class,
+        
+        
         /*
          * Package Service Providers...
          */
@@ -193,7 +194,7 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'PDF' => niklasravnsborg\LaravelPdf\Facades\Pdf::class,
+        
     ])->toArray(),
 
 ];
