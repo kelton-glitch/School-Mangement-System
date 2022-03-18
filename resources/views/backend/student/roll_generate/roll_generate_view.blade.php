39dacfdd2b0b2c43eca2040153728f043c7e3f35
FreshDev <billyhans90@gmail.com>
2022-03-18 10:27:15 +0100

    Student roll generate part 2

--- resources/views/backend/student/roll_generate/roll_generate_view.blade.php
+++ resources/views/backend/student/roll_generate/roll_generate_view.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
 <div class="content-wrapper">
@@ -28,10 +29,10 @@
                                         <div class="form-group">
                                             <h5>Year <span class="text-danger"> </span></h5>
                                             <div class="controls">
-                                                <select name="year_id" required="" class="form-control">
+                                                <select name="year_id" id="year_id" required="" class="form-control">
                                                     <option value="" selected="" disabled="">Select Year</option>
                                                     @foreach($years as $year)
-                                                    <option value="{{ $year->id }}" {{ (@$year_id == $year->id)? "selected":"" }}>{{ $year->name }}</option>
+                                                    <option value="{{ $year->id }}">{{ $year->name }}</option>
                                                     @endforeach
 
                                                 </select>
@@ -45,10 +46,10 @@
                                         <div class="form-group">
                                             <h5>Class <span class="text-danger"> </span></h5>
                                             <div class="controls">
-                                                <select name="class_id" required="" class="form-control">
+                                                <select name="class_id" id="class_id" required="" class="form-control">
                                                     <option value="" selected="" disabled="">Select Class</option>
                                                     @foreach($classes as $class)
-                                                    <option value="{{ $class->id }}" {{ (@$class_id == $class->id)? "selected":"" }}>{{ $class->name }}</option>
+                                                    <option value="{{ $class->id }}">{{ $class->name }}</option>
                                                     @endforeach
 
                                                 </select>
@@ -65,6 +66,26 @@
 
                                 ///////// Roll generate table /////////
 
+                                <div class="row d-none" id="roll-generate">
+                                    <div class="col-md-12">
+                                        <table class="table table-bordered table-striped" style="width: 100%">
+                                        <thead>
+                                            <tr>
+                                                <th>ID No</th>
+                                                <th>Student Name</th>
+                                                <th>Father Name</th>
+                                                <th>Gender</th>
+                                                <th>Roll</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody id="roll-generate-tr">
+
+                                        </tbody>
+                                    </table>
+                                    </div>
+                                </div>
+                                <input type="submit" class="btn btn-info" value="Roll Generator">
+
                             </form>
 
                 </div>
@@ -76,6 +97,33 @@
 
     </div>
 </div>
+
+<script type="text/javascript">
+  $(document).on('click','#search',function(){
+    var year_id = $('#year_id').val();
+    var class_id = $('#class_id').val();
+     $.ajax({
+      url: "{{ route('student.registration.getstudents')}}",
+      type: "GET",
+      data: {'year_id':year_id,'class_id':class_id},
+      success: function (data) {
+        $('#roll-generate').removeClass('d-none');
+        var html = '';
+        $.each( data, function(key, v){
+          html +=
+          '<tr>'+
+          '<td>'+v.student.id_no+'<input type="hidden" name="student_id[]" value="'+v.student_id+'"></td>'+
+          '<td>'+v.student.name+'</td>'+
+          '<td>'+v.student.fname+'</td>'+
+          '<td>'+v.student.gender+'</td>'+
+          '<td><input type="text" class="form-control form-control-sm" name="roll[]" value="'+v.roll+'"></td>'+
+          '</tr>';
+        });
+        html = $('#roll-generate-tr').html(html);
+      }
+    });
+  });
+</script>
 
 
 
