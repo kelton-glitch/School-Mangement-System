440ef2c4a8049f3691aaa315480b9e3446e6961f
FreshDev <billyhans90@gmail.com>
2022-03-21 21:32:36 +0100

    Solved the bug, I think the assign subject feature is done

--- app/Models/AssignSubject.php
+++ app/Models/AssignSubject.php
@@ -7,5 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class AssignSubject extends Model
 {
-    use HasFactory;
+     public function student_class(){
+   	return $this->belongsTo(StudentClass::class,'class_id','id');
+   }
+
+ public function school_subject(){
+   	return $this->belongsTo(SchoolSubject::class,'subject_id','id');
+   }
+
 }
